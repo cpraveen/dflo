dab832cfe6a8748cd6d66e91c537096b8b382aa4
cpraveen <cpraveen@gmail.com>
2014-03-02 17:29:06 +0530

    Added command line argument to set number of threads

--- src/main.cc
+++ src/main.cc
@@ -12,17 +12,19 @@ using namespace dealii;
 // line.
 int main (int argc, char *argv[])
 {
-  deallog.depth_console(0);
-  if (argc != 2)
-    {
-      std::cout << "Usage:" << argv[0] << " input_file" << std::endl;
-      std::exit(1);
-    }
-
-  try
+   deallog.depth_console(0);
+   if (argc != 2 || argc != 3)
    {
-      Utilities::System::MPI_InitFinalize mpi_initialization (argc, argv);
-
+      std::cout << "Usage:" << argv[0] << " input_file" << std::endl;
+      std::cout << "      " << argv[0] << " input_file  num_threads" << std::endl;
+      std::exit(1);
+   }
+   unsigned int n_threads = 1;
+   if(argc==3) n_threads = atoi(argv[2]);
+   
+   try
+   {
+      Utilities::System::MPI_InitFinalize mpi_initialization (argc, argv, n_threads);
       ParameterHandler prm;
       Parameters::AllParameters<2>::declare_parameters (prm);
       bool status = prm.read_input (argv[1], true);
@@ -38,29 +40,29 @@ int main (int argc, char *argv[])
       std::cout << "Elapsed CPU time : " << timer()/60 << " min.\n";
       std::cout << "Elapsed wall time: " << timer.wall_time()/60 << " min.\n";
    }
-  catch (std::exception &exc)
-    {
+   catch (std::exception &exc)
+   {
       std::cerr << std::endl << std::endl
-		          << "----------------------------------------------------"
+                << "----------------------------------------------------"
                 << std::endl;
       std::cerr << "Exception on processing: " << std::endl
-		          << exc.what() << std::endl
-		          << "Aborting!" << std::endl
-		          << "----------------------------------------------------"
-		          << std::endl;
+                << exc.what() << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
       return 1;
-    }
-  catch (...)
-    {
+   }
+   catch (...)
+   {
       std::cerr << std::endl << std::endl
-		          << "----------------------------------------------------"
-		          << std::endl;
+                << "----------------------------------------------------"
+                << std::endl;
       std::cerr << "Unknown exception!" << std::endl
-		          << "Aborting!" << std::endl
-		          << "----------------------------------------------------"
-		          << std::endl;
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
       return 1;
-    };
-
-  return 0;
+   };
+   
+   return 0;
 }
