f4ac96ef70bf281e5c3cef26a9957a5acd7f2907
juanpablogallego <jpgalleg@gmail.com>
2014-11-25 14:44:41 +0100

    eliminating useless files

--- src_mpi/CMakeFiles/runclean.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/runclean"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/runclean.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
