f098f34de46bc8724cd183de4ca9998418141f9c
juanpablogallego <jpgalleg@gmail.com>
2014-12-17 16:13:24 +0100

    MHD directory and MHD structure

--- src_mhd/CMakeFiles/release.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/release"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/release.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
